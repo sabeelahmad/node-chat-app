0d390824f7f81521d786f1d4c74f34933b56bb97
Sabeel Ahmad <sabeelbasharat@gmail.com>
2018-12-28 16:42:42 +0530

    Home page added

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,6 +2,22 @@
 // This initializes a connection request by the client to the server
 var socket = io();
 
+function scrollToBottom () {
+  // Selectors
+  var messages = $('#msgs');
+  var newMessage = messages.children('li:last-child');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 // When connection is established by client with server
 socket.on('connect', function() {
     console.log('connected to server');
@@ -29,6 +45,7 @@ socket.on('newMessage', function(message) {
 
   $('#msgs').append(html);
 
+  scrollToBottom();
    // var li = $('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
   // $('#msgs').append(li);
@@ -94,6 +111,7 @@ socket.on('newLocationMessage', function (message) {
 
   $('#msgs').append(html);
 
+  scrollToBottom();
   // var li = $('<li></li>');
   // var a = $('<a target="_blank">My Current Location</a>')
   // li.text(`${message.from} ${formattedTime}: `);
